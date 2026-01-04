fbac27d1ef3b5d7d5269bdfcf19307cbf1de1c6a
achraf <bastamyachraf@gmail.com>
2026-01-04 14:55:48 +0100

    first commit

---
+++ src/test/java/com/example/Point/of/sale/PointOfSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Point.of.sale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniquePointOfSaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
